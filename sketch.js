4723bad2592b69441019fc2780ffd44c4374c1ac
devsilverfox-ai <dev.silverfox@gmail.com>
2025-07-18 10:49:55 -0400

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(400, 400);
+  background(220);
+}
+
+function draw() {
+
+    var city1 = ['March', 51, 53, 69, 71, 'April', 53, 57, 71, 74, 'May', 57, 60, 74, 75];
+    var city2 = ['March', 40, 64, 44, 71, 'April', 62, 65, 50, 77,'May', 75, 60, 74, 75];
+    text(city1, 25, 50);
+    text(city2, 25, 100);
+}
